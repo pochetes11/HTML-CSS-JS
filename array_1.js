b3d6c8aaf623a5c2879f67a1d90ef9de0a3127d6
pochetes11 <matiasarielmosquera@gmail.com>
2023-09-05 14:07:11 -0300

    array_1

---
+++ array_1.js
@@ -0,0 +1,7 @@
+//¿Qué se imprimirá en la consola al ejecutar el siguiente código?
+
+const numbers = [1, 2, 3, 4, 5];
+numbers[0] = 10;
+numbers[2] = 30;
+console.log(numbers);
+// R: 10, 2, 30, 4, 5
