513063bf25af815912e41eb1c4fe9d3d658bd4d7
pochetes11 <matiasarielmosquera@gmail.com>
2023-09-05 16:42:31 -0300

    objetos2

--- objeto_2.js
+++ objeto_2.js
@@ -1,7 +1,7 @@
 /*Ejercicio 1: 
 ¿Qué se imprimirá en la consola al ejecutar el siguiente código? y por que? 
 */ const persona = {
-  nombre: "Roma",
+  nombre: "Luna",
   edad: 30,
   hobbies: ["leer", "salir", "futbol"],
 };
@@ -12,3 +12,23 @@ console.log(weight, height);
 // Respuesta: se imprimira 70 y 180
 // porque le dieron a las constantes
 // ese valor por defecto
+
+// 2- ¿Qué se imprimirá en la consola al ejecutar
+// el siguiente código? Y por que?
+
+const person = {
+  name: "Sol",
+  age: 30,
+  hobbies: ["leer", "sali", "futbol"],
+};
+
+const { age: personAge } = person;
+console.log(age);
+
+// Respuesta: Se imprimira la edad porque el
+// codigo solicita la edad
+
+// 3- ¿Cómo se llama el atajo que nos permite recuperar
+// el valor de una propiedad de un objeto?
+
+// Respuesta:
